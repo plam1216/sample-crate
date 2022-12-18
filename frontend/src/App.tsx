d8446390651e88fd7bdd36dea4dd461daf925de8
Peter Lam <plam1216@gmail.com>
2022-12-18 04:24:01 -0500

    get discogsToken from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 import { auth } from './services/firebase'
 import { User as FirebaseUser } from 'firebase/auth'
 
 import Header from './Components/Header/Header';
-import { Container } from 'react-bootstrap';
+import Main from './Pages/Main/Main';
 
-interface Song {
-  name: string
-  album: string
-  artist: string
-  release_date: string
-  duration: number
-}
+import { User } from './types';
 
-interface User extends Document {
-  username?: string
-  email: string
-  pfp?: string
-  playlist?: Song[]
-}
 
 function App() {
   const [fbUser, setfbUser] = useState<FirebaseUser | null>(null)
-  const [dbUser, setdbUser] = useState<User | null>(null)
+  const [allUsers, setAllUsers] = useState<User[]>({} as User[])
 
-  // console.log(fbUser)
-  // console.log("MongoDB", dbUser)
+  const [discogsToken, setDiscogsToken] = useState<string | null>(null)
 
   const URL = "http://localhost:4000/users/"
+  const TOKEN_URL = "http://localhost:4000/discogstoken/"
+
+  // console.log('./App token', discogsToken)
+  // console.log('./App allusers', allUsers)
+
+  // get discogs token
+  const getDiscogsToken = async () => {
+    const response = await fetch(TOKEN_URL)
+    const data = await response.text()
+
+    setDiscogsToken(data)
+  }
 
   // get all users from MongoDB
-  const getUsers = async () => {
+  const getUsers = async (): Promise<void> => {
     const response = await fetch(URL)
-
     const data = await response.json()
 
-    setdbUser(data)
+    setAllUsers(data)
   }
 
   // create a user in MongoDB using Firebase login info
-  const createUser: (fbUser: FirebaseUser | null) => Promise<void> = async (fbUser) => {
-    // send a JWT from frontend to backend to access Firebase login info in backend
-    // see index.ts line 31
+  const createUser: (fbUser: FirebaseUser | null) => Promise<void> = async (fbUser): Promise<void> => {
+
+    // send a JWT from frontend to backend to access Firebase login info in backend/index.ts
     const token = await fbUser?.getIdToken()
-    // console.log(token)
 
     await fetch(URL, {
       method: "POST",
@@ -54,15 +52,18 @@ function App() {
         "Authorization": "Bearer " + token
       }
     })
+
     getUsers()
   }
 
   useEffect(() => {
+    getDiscogsToken()
     getUsers()
 
     // onAuthStateChanged triggers when someone logs in or logs out
     const unsubscribe = auth.onAuthStateChanged(fbUser => {
-      // set fbUser to logged in user upon login or null if logging out
+
+      // set fbUser to logged in user upon login or null on logout
       setfbUser(fbUser)
 
       // createUser in MongoDB if user logged in
@@ -76,6 +77,11 @@ function App() {
       <Header
         getUsers={getUsers}
         fbUser={fbUser}
+      />
+      <Main
+        allUsers={allUsers}
+        fbUser={fbUser}
+        discogsToken={discogsToken}
       />
     </Container>
   );

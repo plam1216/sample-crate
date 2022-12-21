30a4b047007a0051320cea0f85aa51e0e53f0a73
Peter Lam <plam1216@gmail.com>
2022-12-20 23:48:08 -0500

    add media queries

--- frontend/src/Components/Playlist/Playlist.tsx
+++ frontend/src/Components/Playlist/Playlist.tsx
@@ -9,14 +9,14 @@ interface PlaylistProps {
 
 const Playlist = (props: PlaylistProps) => {
     return (
-        <Container className='playlist'>
+        <Container fluid className='playlist'>
             {props.currUserPlaylist.map((song) => {
                 return (
                     <Row className='playlist-song'>
-                        <Col md={{ span: 1}}>
-                            <img src={song.YTthumbnail} style={{borderRadius: 5}}/>
+                        <Col xs={{ span: 12 }} sm={{ span: 2 }} md={{ span: 2 }} lg={{ span: 1 }}>
+                            <img src={song.YTthumbnail} style={{ borderRadius: 5 }} className='YTthumbnail' />
                         </Col>
-                        <Col md={{ span: 10, offset: 1 }} className='playlist-song-info'>
+                        <Col xs={{ span: 7, offset: 2 }} sm={{ span: 8, offset: 2 }} md={{ span: 2, offset: 1 }} lg={{ span: 10, offset: 1 }} className='playlist-song-info'>
                             {song.YTtitle}
                             <br />
                             {song.year}

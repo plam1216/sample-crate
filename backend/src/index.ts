da83caebf41da7b7d4fc40393d452759e8ace25f
Peter Lam <plam1216@gmail.com>
2022-12-21 23:26:07 -0500

    add firebase keys

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -39,7 +39,17 @@ let admin = require("firebase-admin");
 let serviceAccount = require("./sample-crate-firebase-adminsdk-e5c0c-586e71e705.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    "type": "service_account",
+    "project_id": "sample-crate",
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+    "client_email": "firebase-adminsdk-e5c0c@sample-crate.iam.gserviceaccount.com",
+    "client_id": "117193948380667927354",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-e5c0c%40sample-crate.iam.gserviceaccount.com"
+
 });
 
 

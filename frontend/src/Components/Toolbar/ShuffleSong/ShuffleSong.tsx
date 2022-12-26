8f31827dd09025cf84e20ad680d90641c6a9be73
Peter Lam <plam1216@gmail.com>
2022-12-26 00:21:49 -0500

    rename file

---
+++ frontend/src/Components/Toolbar/ShuffleSong/ShuffleSong.tsx
@@ -0,0 +1,32 @@
+import { analytics } from '../../../services/firebase'
+import { logEvent } from 'firebase/analytics'
+
+import { Shuffle } from 'react-bootstrap-icons'
+
+import { SearchParams } from '../../../types'
+
+
+interface ShuffleSongProps {
+  filteredSearch: SearchParams
+  getVideoURL: () => void
+  getRandomDiscogsSong: () => void
+  getFilteredDiscogsSong: (genre: string, year?: number) => void
+}
+
+const ShuffleSong = (props: ShuffleSongProps) => {
+  return (
+    <div>
+      <Shuffle
+        onClick={() => {
+          props.filteredSearch ? props.getFilteredDiscogsSong(props.filteredSearch.genre, props.filteredSearch.year) : props.getRandomDiscogsSong()
+          props.getVideoURL()
+          logEvent(analytics, "shuffle", {})
+        }}
+        size={40}
+        style={{ cursor: 'pointer' }}
+      />
+    </div>
+  )
+}
+
+export default ShuffleSong

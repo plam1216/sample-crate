b913a3e8fdc425a4abc288a3e321fe3371bf6a9e
Peter Lam <plam1216@gmail.com>
2022-12-26 00:25:20 -0500

    remove background color

--- frontend/src/Components/YouTubeEmbed/YouTubeEmbed.tsx
+++ frontend/src/Components/YouTubeEmbed/YouTubeEmbed.tsx
@@ -18,7 +18,6 @@ const YouTubeEmbed = (props: YouTubeEmbedProps) => {
                         width="853"
                         height="480"
                         src={YTvideo}
-                        // frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
                         title="Embedded youtube"
